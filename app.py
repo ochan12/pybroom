68ab38c28b8af8f4810e234214a1a3a29527d5a7
mateoochandorena <mateochando@gmail.com>
2020-10-26 21:53:55 +0100

    Add logs, clean code, fix bug

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import logging
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+
 from config.env import DESTINATION_AUTH, DESTINATION_URL
 import os, base64
 from functools import wraps
@@ -10,18 +14,22 @@ app.config.update(
     CELERY_BROKER_URL=os.getenv('CELERY_BROKER_URL')
 )
 celery = make_celery(app)
+logger = logging.getLogger(__name__)
+
 
 @app.route('/', methods=['GET'])
 def hello():
-		return "This is PyBroom \nPlease configure me and start posting information so I can clean!\n"
+    return "This is PyBroom \nPlease configure me and start posting information so I can clean!\n"
+
 
 def check(authorization_header):
     username = os.getenv('USER_AUTH')
     password = os.getenv('PASSWORD_AUTH')
     encoded_uname_pass = authorization_header.split()[-1]
     encoded_local = base64.b64encode((username + ":" + password).encode('utf-8')).decode('utf-8')
-    if encoded_uname_pass == encoded_local :
+    if encoded_uname_pass == encoded_local:
         return True
+
 
 def login_required(f):
     @wraps(f)
@@ -31,41 +39,49 @@ def login_required(f):
             return f(*args, **kwargs)
         else:
             return "Authentication failed\n", 401
-        return f(*args, **kwargs)
     return decorated  
 
+
 def process_object(new_object):
-    print("Process Object")
+    logger.info("Process Object")
     valid_user = cleaner.is_valid_user(new_object)
     if valid_user['result'] is True:
+        logger.info("Is valid user")
         valid_content = cleaner.is_valid_content(new_object)
         if valid_content['result'] is True:
+            logger.info("Content is okey")
             clean_object = cleaner.clean_objects(new_object)
             clean_object = cleaner.delete_project_fields(clean_object)
             headers = {
                 'Authorization': DESTINATION_AUTH,
                 'Content-Type': 'application/json'
             }
-            req = requests.post(url=DESTINATION_URL, data=json.dumps(clean_object), headers=headers,verify=False)
-            print("Sent:"+str(new_object)+" - Code: "+str(req.status_code))
+            req = requests.post(url=DESTINATION_URL,
+                                data=json.dumps(clean_object),
+                                headers=headers,
+                                verify=False)
+            logger.info("Sent:"+str(new_object)+" - Code: "+str(req.status_code))
             return req.text
         else:
+            logger.error("Invalid content")
             return dict({'reason': "Invalid content", 'success': False})
     else:
+        logger.error("Invalid user")
         return dict({'reason': "Invalid user", 'success': False})
 
 
 @app.route('/clean', methods=["POST"])    
 @login_required
 def clean():
-    print("New Object to clean!!")
-    new_object =  request.json
+    logger.info("New Object to clean!!")
+    new_object = request.json
     try:
         clean_task.delay(new_object)
     except Exception as e:
         print(e)
         return jsonify({'success': False, 'reason': e}), 500
-    return jsonify({'succes': True}), 200
+    return jsonify({'success': True}), 200
+
 
 @celery.task()
 def clean_task(new_object):
@@ -73,7 +89,5 @@ def clean_task(new_object):
     return 'Done'
 
 
-
 if __name__ == "__main__":
     app.run(port=8888, host='0.0.0.0')
-    
--- cleaner/cleaner.py
+++ cleaner/cleaner.py
@@ -1,38 +1,66 @@
-import requests
+import logging
+
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 import re
 
+logger = logging.getLogger(__name__)
+
+
 def is_valid_user(dirty_object: dict):
-    print("Checking User")
-    if dirty_object.__contains__('author') and "banned_users" in dirty_object:
+    logger.info("Checking User")
+    if "author" in dirty_object and "banned_users" in dirty_object:
+        logger.info("Checking if author is banned users")
         banned_users = dirty_object['banned_users']
-        if banned_users.__contains__(dirty_object['author']):
-                return dict({ "result": False, "reason": 'User Banned'})
-        return dict({ "result": True, "reason": 'User is not banned'})
+        if dirty_object['author'] in banned_users:
+            logger.error("User banned")
+            return dict({"result": False, "reason": 'User Banned'})
+        logger.info("User is not banned")
+        return dict({"result": True, "reason": 'User is not banned'})
     else:
-        return dict({ "result": False, "reason": 'Author field in dirty object wansn\'t found'})
+        return dict({"result": True, "reason": 'No author field or banned_users'})
+
 
 def is_valid_content(dirty_object: dict):
+    logger.info("Checking content")
     if "banned_words" in dirty_object:
         for bannedWord in dirty_object['banned_words']:
-            lookup_content = re.search(bannedWord,dirty_object['content'])
+            lookup_content = re.search(bannedWord, dirty_object['content'])
             if lookup_content:
+                logger.error("Content banned")
                 return dict({"result": False, "reason": "Banned content found"})
     return dict({"result": True, "reason": "Content is legit"})
 
+
 def delete_project_fields(dirty_object: dict):
-    for field in ["twitterUsers", "facebookUsers", "instagramUsers", "youtubeUsers", "banned_users", "banned_words", "keywords", "project_name", "project_slug", "createdAt", "index", "tags"]:
-        if dirty_object.__contains__(field):
+    for field in [
+        "twitterUsers",
+        "facebookUsers",
+        "instagramUsers",
+        "youtubeUsers",
+        "banned_users",
+        "banned_words",
+        "keywords",
+        "project_name",
+        "project_slug",
+        "createdAt",
+        "index",
+        "tags",
+        "from_date",
+        "to_date"
+    ]:
+        if field in dirty_object:
             dirty_object.__delitem__(field)
     return dirty_object
 
-def clean_objects(dirty_object:dict):
+
+def clean_objects(dirty_object: dict):
     clean_object = dirty_object
     for field in ['content', 'text', 'title']:
-        if clean_object.__contains__(field):
-            print("Old field")
-            print(clean_object[field])
+        if field in clean_object:
+            logger.info("Old field")
+            logger.info(clean_object[field])
             clean_object[field] = re.sub('["\n", "\t", "\'", "\""]', ' ', dirty_object[field])
-            print("New field")
-            print(clean_object[field])
+            logger.info("New field")
+            logger.info(clean_object[field])
     return clean_object
-        
